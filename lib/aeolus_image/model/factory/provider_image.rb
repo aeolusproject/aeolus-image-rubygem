d9c13a68117cf5d122df35677cf39f20c994fdac
Martyn Taylor <mtaylor@redhat.com>
2011-09-30 09:43:06 +0100

    Added Push via factory REST API

--- lib/aeolus_image/model/factory/provider_image.rb
+++ lib/aeolus_image/model/factory/provider_image.rb
@@ -5,6 +5,18 @@ module Aeolus
         def self.status(id)
           Aeolus::Image::Factory::Builder.find(id).status
         end
+
+        def save
+          post(post_url)
+        end
+
+        def post_url
+          "images/" + @attributes['image_id'] + "/builds/" + @attributes['build_id'] + "/target_images/" + @attributes['target_image_id'] + "/provider_images"
+        end
+
+        def custom_method_new_element_url(method_name, options = {})
+          self.class.prefix(prefix_options) + post_url
+        end
       end
     end
   end
