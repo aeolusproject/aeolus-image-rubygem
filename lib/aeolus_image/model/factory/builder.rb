289dcbf0bf53d24c9da6532fec8c6b6219187e33
Jan Provaznik <jprovazn@redhat.com>
2011-11-29 10:36:38 +0100

    Added builder helper methods
    These methods simplify finding of active builds and pushes

--- lib/aeolus_image/model/factory/builder.rb
+++ lib/aeolus_image/model/factory/builder.rb
@@ -2,6 +2,16 @@ module Aeolus
   module Image
     module Factory
       class Builder < Base
+        ACTIVE_STATES = ['FAILED', 'COMPLETED']
+
+        def find_active_build(build_id, target)
+          builders.find {|b| !ACTIVE_STATES.include?(b.status) && b.operation == 'build' && b.build_id == build_id}
+        end
+
+        def find_active_push(target_image_id, provider, account)
+          builders.find {|b| !ACTIVE_STATES.include?(b.status) && b.operation == 'push' && b.target_image_id == target_image_id &&
+                              b.provider == provider && b.provider_account_identifier == account}
+        end
       end
     end
   end
