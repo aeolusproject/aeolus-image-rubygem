36298d123ca39ec3c7dd0540e71ff831f18234c3
Scott Seago <sseago@redhat.com>
2012-02-23 18:10:14 -0500

    Bug 796783: fixes lingering errors w/ image import
    https://bugzilla.redhat.com/show_bug.cgi?id=796783

--- lib/aeolus_image/import.rb
+++ lib/aeolus_image/import.rb
@@ -14,7 +14,7 @@
 
 module Aeolus
   module Image
-    def self.import(provider_name, deltacloud_driver, image_id, account_id, xml=nil)
+    def self.import(provider_name, deltacloud_driver, image_id, account_id, environment, xml=nil)
       xml ||= "<image><name>#{image_id}</name></image>"
       image = Factory::Image.new(
         :target_name => deltacloud_driver,
@@ -25,7 +25,8 @@ module Aeolus
       image.save!
       # Set the provider_account_id on the image
       iwhd_image = Warehouse::Image.find(image.id)
-      # Set the account on the provider image
+      iwhd_image.set_attr("environment", environment)
+     # Set the account on the provider image
       # This assumes (as is currently correct) that there will only be one provider image for imported images
       pimg = iwhd_image.provider_images.first
       pimg.set_attr('provider_account_identifier', account_id)
