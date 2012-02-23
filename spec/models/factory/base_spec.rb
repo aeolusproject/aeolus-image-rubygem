91a9e528f61826657268ac127d501a6727e13dff
Petr Blaho <pblaho@redhat.com>
2012-02-23 15:14:11 +0100

    Resolve VCR problem with matching  requests
    Refactored some tests code to bypass VCR request matching problem
    
    https://bugzilla.redhat.com/show_bug.cgi?id=791216

--- spec/models/factory/base_spec.rb
+++ spec/models/factory/base_spec.rb
@@ -42,41 +42,53 @@ describe Aeolus::Image::Factory::Base do
     Aeolus::Image::Factory::Base.use_oauth?.should be_true
   end
 
-  it "should succeed with valid OAuth credentials" do
-    Aeolus::Image::Factory::Base.config = {
-      :site => 'http://127.0.0.1:8075/imagefactory',
-      :consumer_key => 'mock-key',
-      :consumer_secret => 'mock-secret'
-    }
-    template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
-    img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
-    img.save!.should be_true
+  context do
+    use_vcr_cassette "oauth_success_valid"
+
+    it "should succeed with valid OAuth credentials" do
+      Aeolus::Image::Factory::Base.config = {
+        :site => 'http://127.0.0.1:8075/imagefactory',
+        :consumer_key => 'mock-key',
+        :consumer_secret => 'mock-secret'
+      }
+      template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
+      img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
+      img.save!.should be_true
+    end
   end
 
-  it "should fail with invalid OAuth credentials" do
-    Aeolus::Image::Factory::Base.config = {
-      :site => 'http://127.0.0.1:8075/imagefactory',
-      :consumer_key => 'mock-key',
-      :consumer_secret => 'wrong-secret'
-    }
-    template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora 2</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
-    img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
-    lambda {
-      img.save!
-    }.should raise_error(ActiveResource::UnauthorizedAccess)
+  context do
+    use_vcr_cassette "oauth_fail_invalid"
+
+    it "should fail with invalid OAuth credentials" do
+      Aeolus::Image::Factory::Base.config = {
+        :site => 'http://127.0.0.1:8075/imagefactory',
+        :consumer_key => 'mock-key',
+        :consumer_secret => 'wrong-secret'
+      }
+      template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora 2</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
+      img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
+      lambda {
+        img.save!
+      }.should raise_error(ActiveResource::UnauthorizedAccess)
+    end
   end
 
-  it "should fail with no OAuth credentials" do
-    Aeolus::Image::Factory::Base.config = {
-      :site => 'http://127.0.0.1:8075/imagefactory'
-    }
-    template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora 3</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
-    img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
-    # ServerError is actually not what _should_ be returned, but a bug means that's what we get at the moment.
-    # When the bug is fixed, we need to update to test for that.
-    lambda {
-      img.save!.should be_false
-    }.should raise_error(ActiveResource::ServerError)
+  context do
+    use_vcr_cassette "oauth_fail_no"
+
+    it "should fail with no OAuth credentials" do
+      Aeolus::Image::Factory::Base.config = {
+        :site => 'http://127.0.0.1:8075/imagefactory'
+      }
+      template = "<template>\n  <name>f14jeos</name>\n  <os>\n    <name>Fedora 3</name>\n    <version>14</version>\n    <arch>x86_64</arch>\n    <install type='url'>\n      <url>http://download.fedoraproject.org/pub/fedora/linux/releases/14/Fedora/x86_64/os/</url>\n    </install>\n  </os>\n  <description>Fedora 14</description>\n</template>\n"
+      img = Aeolus::Image::Factory::Image.new(:targets => 'ec2', :template => template)
+      # ServerError is actually not what _should_ be returned, but a bug means that's what we get at the moment.
+      # When the bug is fixed, we need to update to test for that.
+      lambda {
+        img.save!.should be_false
+      }.should raise_error(ActiveResource::ServerError)
+    end
   end
 
 end
--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -24,11 +24,3 @@ VCR.config do |config|
   config.allow_http_connections_when_no_cassette = true
 end
 
-ActiveResource::Connection.class_eval do
-  def request(method, path, *args)
-    # We want to add :body, but not :headers because they're too fickle with OAuth
-    VCR.use_cassette('oauth', :record => :new_episodes, :match_requests_on => [:method, :uri, :body]) do
-      request_with_oauth(method, path, *args)
-    end
-  end
-end
